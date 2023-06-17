472f193f187e16a60330bcea94793d1b9c06e4b3
Christofer Cousins <77406318+csc530@users.noreply.github.com>
2023-06-17 09:42:06 -0400

    init

---
+++ powershell/completers.ps1
@@ -0,0 +1,6 @@
+# volta completions: javascript tool manager
+(& volta completions powershell) | Out-String | Invoke-Expression
+# my beatful markdown viewer
+glow completion powershell | Out-String | Invoke-Expression
+# terminal prompt themeP
+oh-my-posh.exe completion powershell | Out-String | Invoke-Expression
---
+++ powershell/setup.ps1
@@ -0,0 +1,4 @@
+Write-Host "Hey there 😘"
+Write-Host "We're going to setup you're powershell to beatufil as I'd like !^_^!"
+Write-Host "Press any key to continue..."
+$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
