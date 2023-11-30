659373c5d167c9b8395e3a2b7e58b55b4817a84b
csc530 <77406318+csc530@users.noreply.github.com>
2023-11-30 18:02:47 -0500

    feat(customize): :lipstick: add colour scheme update terminal
    create autodarkmode script to switch windows terminal schemes

---
+++ AutoDarkMode/themeSwitch.ps1
@@ -0,0 +1,20 @@
+﻿# Parameter help description
+param (
+    [Parameter(Position = 0, Mandatory = $false, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, HelpMessage = 'The type of theme to use.')]
+    [ValidateSet('light', 'dark')]
+    $theme
+)
+Import-Module C:\Users\legor\.config\powershell\module\module.psd1
+
+if ($null -eq $theme) {
+    $theme = (Get-Item HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize).GetValue('AppsUseLightTheme', 1) ? 'light' : 'dark'
+}
+
+if ($theme -eq 'dark') {
+    Set-WindowsTerminalScheme -preview -name 'Catppuccin Mocha'
+    Set-WindowsTerminalScheme -name 'Catppuccin Mocha'
+}
+else {
+    Set-WindowsTerminalScheme -preview -name 'Catppuccin Latte'
+    Set-WindowsTerminalScheme -name 'Catppuccin Latte'
+}
