3c046c416416f8c105ba01f92db09fc2e7d6c1f7
csc530 <77406318+csc530@users.noreply.github.com>
2024-04-01 09:24:34 -0400

    re add zoxide to pwsh profile

--- powershell/environment/hooks.ps1
+++ powershell/environment/hooks.ps1
@@ -13,6 +13,7 @@ Write-Host "`rSetting up hooks..." -NoNewline
 # scoop search - faster than default speedup
 # Invoke-Expression (&scoop-search --hook)
 jump shell pwsh | Out-String | Invoke-Expression
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
 
 
 Write-Host "✅"
