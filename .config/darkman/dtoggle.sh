5af3330eb08e262d1814c5f325517f16f62ae00c
csc530 <77406318+csc530@users.noreply.github.com>
2025-08-16 00:51:27 -0400

    update mako and waybar in darkman

--- .config/darkman/dtoggle.sh
+++ .config/darkman/dtoggle.sh
@@ -1,18 +1,28 @@
 #!/bin/sh
 
+ln -sf ./mocha.gtk.css  ~/.config/palette/style.css
+
 alias inkcat="deno run --allow-env --allow-read --allow-sys npm:@catppuccin/inkcat"
 # https://stackoverflow.com/a/2453056/16929246
-colour=$(inkcat mocha | tail --lines=26 | grep --extended-regexp --invert-match "(text|overlay|surface|base|mantle|crust)" | shuf --head-count=1 | sed -r 's/\<./\U&/g')
+colour=$(inkcat mocha | tail --lines=26 | grep --extended-regexp --invert-match "(text|overlay|surface|base|mantle|crust)" | shuf --head-count=1 | sed -r 's/\<./\U&/g' | tr -d '[:space:]')
 
 gsettings set org.gnome.desktop.interface color-scheme prefer-dark
 gsettings set org.gnome.desktop.interface gtk-theme Pop-dark
 
-ln -sf ./mocha/style.css  ~/.config/wofi/style.css 
+ln -sf ./mocha/style.css  ~/.config/wofi/style.css
 
 cursorTheme="Catppuccin Mocha $colour"
-hyprctl setcursor $cursortheme 24
-gsettings set org.gnome.desktop.interface cursor-theme $cursorTheme
-export HYPRCURSOR_THEME=$cursorTheme
+hyprctl setcursor "$cursortheme" 24
+gsettings set org.gnome.desktop.interface cursor-theme "$cursorTheme"
+export HYPRCURSOR_THEME="$cursorTheme"
 export HYPRCURSOR_SIZE=24
 
-echo $HYPRLAND_INSTANCE_SIGNATURE
+# echo $HYPRLAND_INSTANCE_SIGNATURE
+
+colour=$(echo $colour | sed -r 's/\<./\L&/g') # lowercase colour variant
+# mako notifications
+ln -sf "catppuccin-mocha/catppuccin-mocha-$colour" ~/.config/mako/colours
+makoctl reload
+
+# waybar
+pidof waybar && kill -s USR2 $(pidof waybar)
--- .config/darkman/ltoggle.sh
+++ .config/darkman/ltoggle.sh
@@ -1,9 +1,11 @@
-#!/bin/sh
+#!/bin/env sh
+
+ln -sf ./latte.gtk.css  ~/.config/palette/style.css
 
 alias inkcat="deno run --allow-env --allow-read --allow-sys npm:@catppuccin/inkcat"
 
 # https://stackoverflow.com/a/2453056/16929246
-colour=$(inkcat latte | tail --lines=26 | grep --extended-regexp --invert-match "(text|overlay|surface|base|mantle|crust)" | shuf --head-count=1 | sed -r 's/\<./\U&/g')
+colour=$(inkcat mocha | tail --lines=26 | grep --extended-regexp --invert-match "(text|overlay|surface|base|mantle|crust)" | shuf --head-count=1 | sed -r 's/\<./\U&/g' | tr -d '[:space:]')
 
 gsettings set org.gnome.desktop.interface color-scheme prefer-light
 gsettings set org.gnome.desktop.interface gtk-theme Pop
@@ -11,9 +13,17 @@ gsettings set org.gnome.desktop.interface gtk-theme Pop
 ln -sf ./latte/style.css  ~/.config/wofi/style.css
 
 cursorTheme="Catppuccin Latte $colour"
-hyprctl setcursor $cursortheme 24
-gsettings set org.gnome.desktop.interface cursor-theme $cursorTheme
-export HYPRCURSOR_THEME=$cursorTheme
+hyprctl setcursor "$cursortheme" 24
+gsettings set org.gnome.desktop.interface cursor-theme "$cursorTheme"
+export HYPRCURSOR_THEME="$cursorTheme"
 export HYPRCURSOR_SIZE=24
 
-echo $HYPRLAND_INSTANCE_SIGNATURE
+# echo $HYPRLAND_INSTANCE_SIGNATURE
+
+colour=$(echo $colour | sed -r 's/\<./\L&/g') # lowercase colour variant
+# mako notifications
+ln -sf "catppuccin-latte/catppuccin-latte-$colour" ~/.config/mako/colours
+makoctl reload
+
+# waybar
+pidof waybar && kill -s USR2 $(pidof waybar)
